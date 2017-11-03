ece15419b5cef491417c27e232fb8b634822ac2b
LeadKiller <hl2epicfan@gmail.com>
2017-11-03 16:01:35 -0700

    Update cl_scoreboard.lua

--- gamemodes/suicidebarrels/gamemode/cl_scoreboard.lua
+++ gamemodes/suicidebarrels/gamemode/cl_scoreboard.lua
@@ -182,9 +182,14 @@ function GM:HUDDrawScoreBoard()
 
 	surface.SetTextColor( 255, 255, 255, 255 )
 	surface.SetFont( "DermaDefault" )
-	surface.SetTextPos(xOffset + (boardWidth / 0.96) - (txWidth/2), y + 7)
+	surface.SetTextPos(xOffset + (boardWidth / 0.96) - (txWidth/2), y + -7)
 	surface.DrawText( "and Updated by Zephruz" )
 	
+		surface.SetTextColor( 255, 255, 255, 255 )
+	surface.SetFont( "DermaDefault" )
+	surface.SetTextPos(xOffset + (boardWidth / 0.96) - (txWidth/2), y + 7)
+	surface.DrawText( "with fixes from Moddage" )
+
 	y = y + txHeight + 4
 	GAMEMODE.ScoreDesign.HeaderY = y - yOffset
 	
